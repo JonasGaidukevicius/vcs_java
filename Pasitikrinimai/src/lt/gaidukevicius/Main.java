137be6410d47fb8a03b2473ec7ffb86986c470c1
Jonas Gaidukevicius <jgaidukevicius@gmail.com>
2018-10-22 17:11:57 +0300

    2018.10.22 iš namų

--- Pasitikrinimai/src/lt/gaidukevicius/Main.java
+++ Pasitikrinimai/src/lt/gaidukevicius/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		//System.out.println(stringMatch("xxcaazz", "xxbaaz"));
 		
+		              
+             System.out.println(0 & 48);
+		
 		
 	}
 	

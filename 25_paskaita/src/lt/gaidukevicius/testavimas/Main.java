b3975fabea7a24d01360cabac8719faefeb6c465
meja_bac <salomejabac@gmail.com>
2018-10-25 21:23:26 +0300

    2018.10.25 po paskaitos vcs

---
+++ 25_paskaita/src/lt/gaidukevicius/testavimas/Main.java
@@ -0,0 +1,10 @@
+package lt.gaidukevicius.testavimas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 25_paskaita/src/lt/gaidukevicius/testavimas/TestMain.java
@@ -0,0 +1,15 @@
+package lt.gaidukevicius.testavimas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMain {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertEquals(1, 1);
+	}
+
+}
